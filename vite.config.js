4d837c1935af9f864ced7b2d7c9f07981cabac77
Mohit Kumar <mohitkumar120904@gmail.com>
2025-09-01 20:52:11 +0530

    Configure GitHub Pages deployment with Vite and GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Ensure this starts and ends with a slash! 
   plugins: [react()],
+  base: '/decarbonize/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
